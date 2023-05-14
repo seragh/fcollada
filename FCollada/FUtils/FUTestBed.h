9e3418ff4b0e8573d61e631edadd6dd027a47dbc
Ralph Sennhauser <ralph.sennhauser@gmail.com>
2023-05-14 09:11:03 +0200

    Fix narrowing conversion in test runner
    Also numeric_limit::max() is a more C++-ish way to say the same.
    
    Signed-off-by: Ralph Sennhauser <ralph.sennhauser@gmail.com>

--- FCollada/FUtils/FUTestBed.h
+++ FCollada/FUtils/FUTestBed.h
@@ -123,7 +123,7 @@ public: \
 	virtual bool RunTest(FUTestBed& testBed, FULogFile& fileOut, bool& __testSuiteDone, size_t testIndex) \
 	{ \
 		switch (testIndex) { \
-			case ~0 : { \
+			case std::numeric_limits<size_t>::max() : { \
 				if (testBed.IsVerbose()) { \
 					fileOut.WriteLine("Running %s...", #suiteName); \
 				}
